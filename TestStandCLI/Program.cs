989619d6edb8a2c2aa23bf859c564c83bc8ee32c
425J <contact@twosheds.blog>
2021-11-21 11:05:59 -0800

    Proper baseline
    https://zone.ni.com/reference/en-XX/help/370052AA-01/tsapiref/infotopics/csharp/
    https://zone.ni.com/reference/en-XX/help/370052AA-01/tsfundamentals/infotopics/tsutil_functions_lib/
    https://zone.ni.com/reference/en-XX/help/370052AA-01/tsfundamentals/infotopics/net_ui_start_shutdown/

--- TestStandCLI/Program.cs
+++ TestStandCLI/Program.cs
@@ -4,11 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NationalInstruments.TestStand.Utility;
+using NationalInstruments.TestStand.Interop.API;
+
 namespace TestStandCLI
 {
     internal class Program
     {
+        private static Engine engine = null;
+        private static SequenceFile sequenceFile = null;
+
+        [STAThread]
         static void Main(string[] args)
+        {
+            // Create application domain, call MainEntryPoint, and
+            // cleanup before return.
+            LaunchTestStandApplicationInNewDomain.LaunchProtected(
+               new LaunchTestStandApplicationInNewDomain.
+                   MainEntryPointDelegateWithArgs(MainEntryPoint),
+               args,
+               "TestStand Application",
+               new LaunchTestStandApplicationInNewDomain.
+                   DisplayErrorMessageDelegate(DisplayErrorMessage));
+        }
+
+        // Main entry point executed in new application domain.
+        private static void MainEntryPoint(string[] args)
+        {
+
+            // Obtain a reference to existing Engine.
+            Program.engine = new Engine();
+            Program.engine.LoadTypePaletteFilesEx(
+               TypeConflictHandlerTypes.ConflictHandler_Prompt, 0);
+
+            // Launch your application here.
+            // e.g., Application.Run(new MainForm());
+
+            // Engine clean up.
+
+            Program.engine = null;
+
+        }
+
+        // Called if exception occurs in MainEntryPoint.
+        private static void DisplayErrorMessage(string caption, string message)
         {
         }
     }
